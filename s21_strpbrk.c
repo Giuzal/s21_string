9644cff56f532f90d449c088ffaea32ed5b7f1d6
Giuzal <giuzalchek@gmail.com>
2024-05-20 23:01:20 +0300

    strpbrk

--- s21_strpbrk.c
+++ s21_strpbrk.c
@@ -1,1 +1,17 @@
 #include "s21_string.h"
+
+char *s21_strpbrk(const char *string, const char *charset){
+    while(*string != '\0')
+    {
+        const char *temp = charset;
+        while (*temp != '\0'){
+            if (*string == *temp){
+                return (char *) string;
+            }
+            temp++;
+        }
+        string++;
+    }
+
+    return S21_NULL;
+}

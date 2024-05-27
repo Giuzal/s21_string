c1afcadafee52fb35df8d63e65ae33e70b2624c7
Giuzalchek <giuzalchek@gmail.com>
2024-05-27 23:29:57 +0300

    tests finally works

---
+++ src/s21_strcat.c
@@ -0,0 +1,12 @@
+#include "s21_string.h"
+
+char *s21_strcat(char *dest, const char *src){
+
+    s21_size_t i = s21_strlen(dest);
+    while (*src != '\0'){
+     dest[i] = *src;
+        src++;
+         i++;
+    }
+return dest;
+}
---
+++ src/s21_strlen.c
@@ -0,0 +1,13 @@
+#include "s21_string.h"
+
+s21_size_t s21_strlen(const char *str)
+{
+    s21_size_t length = 0;
+    if(str){
+        while (str[length] != '\0'){
+            length++;
+        }
+    }
+    return length;
+}
+
